9d642bfecf492808db8e27069a0ef5253aeff175
Wenny Hidayat <wenny.h@live.com>
2017-02-08 15:02:27 +1100

    Added crawler which pipes results into own fields

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -10,5 +10,7 @@ import scrapy
 
 class TutorialItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    quote = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()
     pass
--- tutorial/tutorial/spiders/example.py
+++ tutorial/tutorial/spiders/example.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.loader import ItemLoader
+from tutorial.items import TutorialItem
 
 class ExampleSpider(scrapy.Spider):
     name = "example"
-    allowed_domains = ["example.com"]
-    start_urls = ['http://example.com/']
+    # allowed_domains = ["example.com"]
+    # start_urls = ['http://example.com/']
+
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/'
+    ]
 
     def parse(self, response):
-        pass
+        l  =ItemLoader(item = TutorialItem(), response = response)
+        l.add_xpath('quote', '//span[@class=\'text\']/text()')
+        l.add_css('author', 'span small::text')
+        # l.add_css('tags', 'div.tags a.tag::text')
+        return l.load_item()
